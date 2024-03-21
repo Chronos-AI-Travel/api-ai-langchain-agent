396125fdd2ba8f18cb380f72d30a4cadc8573908
joshuasparkes <joshsparkes6@gmail.com>
2024-03-21 20:50:04 +0000

    comment out scripts

--- practice_scripts/agentIteration.py
+++ practice_scripts/agentIteration.py
@@ -1,78 +1,78 @@
-from langchain.agents import AgentType, initialize_agent
-from langchain.chains import LLMMathChain
-from langchain_core.pydantic_v1 import BaseModel, Field
-from langchain_core.tools import Tool
-from langchain_openai import ChatOpenAI
+# from langchain.agents import AgentType, initialize_agent
+# from langchain.chains import LLMMathChain
+# from langchain_core.pydantic_v1 import BaseModel, Field
+# from langchain_core.tools import Tool
+# from langchain_openai import ChatOpenAI
 
-llm = ChatOpenAI(temperature=0, model="gpt-4")
-llm_math_chain = LLMMathChain.from_llm(llm=llm, verbose=True)
+# llm = ChatOpenAI(temperature=0, model="gpt-4")
+# llm_math_chain = LLMMathChain.from_llm(llm=llm, verbose=True)
 
-primes = {998: 7901, 999: 7907, 1000: 7919}
-
-
-class CalculatorInput(BaseModel):
-    question: str = Field()
+# primes = {998: 7901, 999: 7907, 1000: 7919}
 
 
-class PrimeInput(BaseModel):
-    n: int = Field()
+# class CalculatorInput(BaseModel):
+#     question: str = Field()
 
 
-def is_prime(n: int) -> bool:
-    if n <= 1 or (n % 2 == 0 and n > 2):
-        return False
-    for i in range(3, int(n**0.5) + 1, 2):
-        if n % i == 0:
-            return False
-    return True
+# class PrimeInput(BaseModel):
+#     n: int = Field()
 
 
-def get_prime(n: int, primes: dict = primes) -> str:
-    return str(primes.get(int(n)))
+# def is_prime(n: int) -> bool:
+#     if n <= 1 or (n % 2 == 0 and n > 2):
+#         return False
+#     for i in range(3, int(n**0.5) + 1, 2):
+#         if n % i == 0:
+#             return False
+#     return True
 
 
-async def aget_prime(n: int, primes: dict = primes) -> str:
-    return str(primes.get(int(n)))
+# def get_prime(n: int, primes: dict = primes) -> str:
+#     return str(primes.get(int(n)))
 
 
-tools = [
-    Tool(
-        name="GetPrime",
-        func=get_prime,
-        description="A tool that returns the `n`th prime number",
-        args_schema=PrimeInput,
-        coroutine=aget_prime,
-    ),
-    Tool.from_function(
-        func=llm_math_chain.run,
-        name="Calculator",
-        description="Useful for when you need to compute mathematical expressions",
-        args_schema=CalculatorInput,
-        coroutine=llm_math_chain.arun,
-    ),
-]
+# async def aget_prime(n: int, primes: dict = primes) -> str:
+#     return str(primes.get(int(n)))
 
-from langchain import hub
 
-prompt = hub.pull("hwchase17/openai-functions-agent")
+# tools = [
+#     Tool(
+#         name="GetPrime",
+#         func=get_prime,
+#         description="A tool that returns the `n`th prime number",
+#         args_schema=PrimeInput,
+#         coroutine=aget_prime,
+#     ),
+#     Tool.from_function(
+#         func=llm_math_chain.run,
+#         name="Calculator",
+#         description="Useful for when you need to compute mathematical expressions",
+#         args_schema=CalculatorInput,
+#         coroutine=llm_math_chain.arun,
+#     ),
+# ]
 
-from langchain.agents import create_openai_functions_agent
+# from langchain import hub
 
-agent = create_openai_functions_agent(llm, tools, prompt)
+# prompt = hub.pull("hwchase17/openai-functions-agent")
 
-from langchain.agents import AgentExecutor
+# from langchain.agents import create_openai_functions_agent
 
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+# agent = create_openai_functions_agent(llm, tools, prompt)
 
-question = "What is the product of the 998th, 999th and 1000th prime numbers?"
+# from langchain.agents import AgentExecutor
 
-for step in agent_executor.iter({"input": question}):
-    if output := step.get("intermediate_step"):
-        action, value = output[0]
-        if action.tool == "GetPrime":
-            print(f"Checking whether {value} is prime...")
-            assert is_prime(int(value))
-        # Ask user if they want to continue
-        _continue = input("Should the agent continue (Y/n)?:\n") or "Y"
-        if _continue.lower() != "y":
-            break
+# agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+# question = "What is the product of the 998th, 999th and 1000th prime numbers?"
+
+# for step in agent_executor.iter({"input": question}):
+#     if output := step.get("intermediate_step"):
+#         action, value = output[0]
+#         if action.tool == "GetPrime":
+#             print(f"Checking whether {value} is prime...")
+#             assert is_prime(int(value))
+#         # Ask user if they want to continue
+#         _continue = input("Should the agent continue (Y/n)?:\n") or "Y"
+#         if _continue.lower() != "y":
+#             break
--- practice_scripts/structuredOutput.py
+++ practice_scripts/structuredOutput.py
@@ -1,102 +1,102 @@
-from langchain_community.document_loaders import TextLoader
-from langchain_community.vectorstores import Chroma
-from langchain_openai import OpenAIEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+# from langchain_community.document_loaders import TextLoader
+# from langchain_community.vectorstores import Chroma
+# from langchain_openai import OpenAIEmbeddings
+# from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-# Load in document to retrieve over
-loader = TextLoader("state_of_the_union.txt")
-documents = loader.load()
+# # Load in document to retrieve over
+# loader = TextLoader("state_of_the_union.txt")
+# documents = loader.load()
 
-# Split document into chunks
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-texts = text_splitter.split_documents(documents)
+# # Split document into chunks
+# text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+# texts = text_splitter.split_documents(documents)
 
-# Here is where we add in the fake source information
-for i, doc in enumerate(texts):
-    doc.metadata["page_chunk"] = i
+# # Here is where we add in the fake source information
+# for i, doc in enumerate(texts):
+#     doc.metadata["page_chunk"] = i
 
-# Create our retriever
-embeddings = OpenAIEmbeddings()
-vectorstore = Chroma.from_documents(texts, embeddings, collection_name="state-of-union")
-retriever = vectorstore.as_retriever()
+# # Create our retriever
+# embeddings = OpenAIEmbeddings()
+# vectorstore = Chroma.from_documents(texts, embeddings, collection_name="state-of-union")
+# retriever = vectorstore.as_retriever()
 
-from langchain.tools.retriever import create_retriever_tool
+# from langchain.tools.retriever import create_retriever_tool
 
-retriever_tool = create_retriever_tool(
-    retriever,
-    "state-of-union-retriever",
-    "Query a retriever to get information about state of the union address",
-)
+# retriever_tool = create_retriever_tool(
+#     retriever,
+#     "state-of-union-retriever",
+#     "Query a retriever to get information about state of the union address",
+# )
 
-from typing import List
+# from typing import List
 
-from langchain_core.pydantic_v1 import BaseModel, Field
-
-
-class Response(BaseModel):
-    """Final response to the question being asked"""
-
-    answer: str = Field(description="The final answer to respond to the user")
-    sources: List[int] = Field(
-        description="List of page chunks that contain answer to the question. Only include a page chunk if it contains relevant information"
-    )
+# from langchain_core.pydantic_v1 import BaseModel, Field
 
 
-import json
+# class Response(BaseModel):
+#     """Final response to the question being asked"""
 
-from langchain_core.agents import AgentActionMessageLog, AgentFinish
-
-
-def parse(output):
-    # If no function was invoked, return to user
-    if "function_call" not in output.additional_kwargs:
-        return AgentFinish(return_values={"output": output.content}, log=output.content)
-
-    # Parse out the function call
-    function_call = output.additional_kwargs["function_call"]
-    name = function_call["name"]
-    inputs = json.loads(function_call["arguments"])
-
-    # If the Response function was invoked, return to the user with the function inputs
-    if name == "Response":
-        return AgentFinish(return_values=inputs, log=str(function_call))
-    # Otherwise, return an agent action
-    else:
-        return AgentActionMessageLog(
-            tool=name, tool_input=inputs, log="", message_log=[output]
-        )
+#     answer: str = Field(description="The final answer to respond to the user")
+#     sources: List[int] = Field(
+#         description="List of page chunks that contain answer to the question. Only include a page chunk if it contains relevant information"
+#     )
 
 
-from langchain.agents import AgentExecutor
-from langchain.agents.format_scratchpad import format_to_openai_function_messages
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_openai import ChatOpenAI
+# import json
 
-prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", "You are a helpful assistant"),
-        ("user", "{input}"),
-        MessagesPlaceholder(variable_name="agent_scratchpad"),
-    ]
-)
+# from langchain_core.agents import AgentActionMessageLog, AgentFinish
 
-llm = ChatOpenAI(temperature=0)
-llm_with_tools = llm.bind_functions([retriever_tool, Response])
 
-agent = (
-    {
-        "input": lambda x: x["input"],
-        # Format agent scratchpad from intermediate steps
-        "agent_scratchpad": lambda x: format_to_openai_function_messages(
-            x["intermediate_steps"]
-        ),
-    }
-    | prompt
-    | llm_with_tools
-    | parse
-)
-agent_executor = AgentExecutor(tools=[retriever_tool], agent=agent, verbose=True)
-agent_executor.invoke(
-    {"input": "what did the president say about ketanji brown jackson"},
-    return_only_outputs=True,
-)
+# def parse(output):
+#     # If no function was invoked, return to user
+#     if "function_call" not in output.additional_kwargs:
+#         return AgentFinish(return_values={"output": output.content}, log=output.content)
+
+#     # Parse out the function call
+#     function_call = output.additional_kwargs["function_call"]
+#     name = function_call["name"]
+#     inputs = json.loads(function_call["arguments"])
+
+#     # If the Response function was invoked, return to the user with the function inputs
+#     if name == "Response":
+#         return AgentFinish(return_values=inputs, log=str(function_call))
+#     # Otherwise, return an agent action
+#     else:
+#         return AgentActionMessageLog(
+#             tool=name, tool_input=inputs, log="", message_log=[output]
+#         )
+
+
+# from langchain.agents import AgentExecutor
+# from langchain.agents.format_scratchpad import format_to_openai_function_messages
+# from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+# from langchain_openai import ChatOpenAI
+
+# prompt = ChatPromptTemplate.from_messages(
+#     [
+#         ("system", "You are a helpful assistant"),
+#         ("user", "{input}"),
+#         MessagesPlaceholder(variable_name="agent_scratchpad"),
+#     ]
+# )
+
+# llm = ChatOpenAI(temperature=0)
+# llm_with_tools = llm.bind_functions([retriever_tool, Response])
+
+# agent = (
+#     {
+#         "input": lambda x: x["input"],
+#         # Format agent scratchpad from intermediate steps
+#         "agent_scratchpad": lambda x: format_to_openai_function_messages(
+#             x["intermediate_steps"]
+#         ),
+#     }
+#     | prompt
+#     | llm_with_tools
+#     | parse
+# )
+# agent_executor = AgentExecutor(tools=[retriever_tool], agent=agent, verbose=True)
+# agent_executor.invoke(
+#     {"input": "what did the president say about ketanji brown jackson"},
+#     return_only_outputs=True,
+# )
